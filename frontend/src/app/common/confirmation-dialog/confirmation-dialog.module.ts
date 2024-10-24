8414fb2534debab0133c7f41305561c0d85cafc2
Collin Giess <cgiess@wilmactech.com>
2024-10-24 12:14:16 -0400

    added a common confirmation dialog component for reusability

---
+++ frontend/src/app/common/confirmation-dialog/confirmation-dialog.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [ConfirmationDialogComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
+  exports: [ConfirmationDialogComponent]
+})
+export class ConfirmationDialogModule { }
