57721db18d0144ca37c26189623696df0f288ccf
Collin Giess <cgiess@wilmactech.com>
2024-11-11 12:39:07 -0500

    added bean validation to ProjectDTO

--- backend/src/main/java/com/senior/project/backend/project/ProjectDTO.java
+++ backend/src/main/java/com/senior/project/backend/project/ProjectDTO.java
@@ -3,6 +3,10 @@ package com.senior.project.backend.project;
 import java.util.Date;
 import java.util.UUID;
 
+import com.senior.project.backend.validation.PastOrPresentDate;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
@@ -14,8 +18,17 @@ import lombok.NoArgsConstructor;
 @Generated
 public class ProjectDTO {
     UUID id;
+    @NotNull(message = "name is required.")
     String name;
+    @NotNull(message = "description is required.")
     String description;
+    @NotNull(message = "startDate is required.")
     Date startDate;
+    @PastOrPresentDate(message = "endDate must not be in the future.")
     Date endDate;
+
+    @AssertTrue(message = "startDate must be before endDate.")
+    public boolean isStartDateBeforeEndDate() {
+        return endDate == null || startDate == null || startDate.before(endDate);
+    }
 }
