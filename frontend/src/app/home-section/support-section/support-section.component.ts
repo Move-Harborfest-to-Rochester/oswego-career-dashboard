6596327477ee61a4d6dee6cb32561937bb283188
Hunter White <hpw6813@gmail.com>
2024-11-12 16:42:24 -0500

    tab content now uniform

--- frontend/src/app/home-section/support-section/support-section.component.ts
+++ frontend/src/app/home-section/support-section/support-section.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-support-section',
   templateUrl: './support-section.component.html',
   styleUrls: ['./support-section.component.less']
 })
-export class SupportSectionComponent {
+export class SupportSectionComponent implements AfterViewInit {
   tabs = [
     { label: 'Faculty Career Mentor' },
     { label: 'BASAC Office' },
     { label: 'Resume & CV Templates' },
     { label: 'LinkedIn Learning' },
     { label: 'Contacts for Support' }
-
   ];
+
   selectedTab = 0;
+
+  @ViewChildren('tabContent') tabContents!: QueryList<ElementRef>;
 
   selectTab(index: number) {
     this.selectedTab = index;
+    this.setMaxHeight();
   }
+
+  ngAfterViewInit() {
+    this.setMaxHeight();
+  }
+
+  setMaxHeight() {
+    const tabBodies = this.tabContents.toArray();
+    // Find the maximum height from all tab contents
+    const maxHeight = Math.max(...tabBodies.map(tab => tab.nativeElement.offsetHeight));
+
+    tabBodies.forEach(tab => {
+      tab.nativeElement.style.minHeight = `${maxHeight}px`;
+    });
+  }
+
 }
