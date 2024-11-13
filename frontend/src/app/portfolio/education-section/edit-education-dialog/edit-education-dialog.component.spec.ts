1af6a9c0b89d77a78ebc4cda82058e6287454399
Collin Giess <cgiess@wilmactech.com>
2024-11-13 14:12:05 -0500

    test(web): added validation tests for EditEducationDialogComponent

--- frontend/src/app/portfolio/education-section/edit-education-dialog/edit-education-dialog.component.spec.ts
+++ frontend/src/app/portfolio/education-section/edit-education-dialog/edit-education-dialog.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,18 +12,19 @@ import { DegreeProgramListInputModule } from './degree-program-list-input/degree
 import { EditEducationDialogComponent } from './edit-education-dialog.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { PortfolioService } from '../../portfolio.service';
-import { Observable, of, throwError } from 'rxjs';
-import { userJSON } from 'src/app/security/auth.service.spec';
-import { User } from 'src/app/security/domain/user';
+import { PortfolioService, DegreeProgramOperation } from '../../portfolio.service';
+import { of, throwError } from 'rxjs';
 import Education, { EducationJSON } from 'src/domain/Education';
 import { YearLevel } from 'src/domain/Milestone';
 import { AuthService } from 'src/app/security/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import { allMajors } from 'src/app/util/major-list';
+import { MultiMajorInputModule } from './multi-major-input/multi-major-input.module';
 
 describe('EditEducationDialogComponent', () => {
   let component: EditEducationDialogComponent;
   let fixture: ComponentFixture<EditEducationDialogComponent>;
+  let majorControl: FormControl<DegreeProgramOperation | null>;
 
   let dialogRefSpy: jasmine.SpyObj<MatDialogRef<EditEducationDialogComponent>>;
   let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
@@ -62,6 +63,7 @@ describe('EditEducationDialogComponent', () => {
         MatInputModule,
         MatSelectModule,
         ReactiveFormsModule,
+        MultiMajorInputModule,
         DegreeProgramListInputModule,
         HttpClientTestingModule,
         MatSnackBarModule,
@@ -77,6 +79,15 @@ describe('EditEducationDialogComponent', () => {
     });
     fixture = TestBed.createComponent(EditEducationDialogComponent);
     component = fixture.componentInstance;
+    component.createForm();
+    majorControl = new FormControl<DegreeProgramOperation>({
+      operation: 'Create',
+      name: allMajors[0],
+      isMinor: false,
+    }, {
+      validators: component.majorValidators
+    });
+    (component.form.get('majors') as FormArray).push(majorControl);
     fixture.detectChanges();
   });
 
@@ -84,38 +95,60 @@ describe('EditEducationDialogComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('validation', () => {
-    it('should validate university id', () => {
-      const universityIdControl = component.form.get('universityId');
+  it('should validate university id', () => {
+    const universityIdControl = component.form.get('universityId');
 
-      universityIdControl?.setValue('');
-      expect(universityIdControl?.valid).toBeTrue();
+    universityIdControl?.setValue('');
+    expect(universityIdControl?.valid).toBeTrue();
 
-      universityIdControl?.setValue('123');
-      expect(universityIdControl?.valid).toBeTrue();
+    universityIdControl?.setValue('123');
+    expect(universityIdControl?.valid).toBeTrue();
 
-      universityIdControl?.setValue('abc');
-      expect(universityIdControl?.valid).toBeFalse();
+    universityIdControl?.setValue('abc');
+    expect(universityIdControl?.valid).toBeFalse();
 
-      universityIdControl?.setValue('e');
-      expect(universityIdControl?.valid).toBeFalse();
+    universityIdControl?.setValue('e');
+    expect(universityIdControl?.valid).toBeFalse();
+  });
+
+  it('should validate gpa', () => {
+    const gpaControl = component.form.get('gpa');
+
+    gpaControl?.setValue('');
+    expect(gpaControl?.valid).toBeTrue();
+
+    gpaControl?.setValue('4.0');
+    expect(gpaControl?.valid).toBeTrue();
+
+    gpaControl?.setValue('5.0');
+    expect(gpaControl?.valid).toBeTrue();
+
+    gpaControl?.setValue('abc');
+    expect(gpaControl?.valid).toBeFalse();
+  });
+
+  it('marks valid major valid', () => {
+    expect(majorControl.hasError('invalidMajor')).toBeFalse();
+  });
+
+  it('marks empty major invalid', () => {
+    majorControl.setValue({ operation: 'Create', name: '', isMinor: false });
+    expect(majorControl.hasError('invalidMajor')).toBeTrue();
+  });
+
+  it('marks empty delete major valid', () => {
+    majorControl.setValue({
+      id: 'cfbbb65f-6e6d-48a5-9007-6577127291ef',
+      operation: 'Delete',
+      name: '',
+      isMinor: false
     });
+    expect(majorControl.hasError('invalidMajor')).toBeFalse();
+  });
 
-    it('should validate gpa', () => {
-      const gpaControl = component.form.get('gpa');
-
-      gpaControl?.setValue('');
-      expect(gpaControl?.valid).toBeTrue();
-
-      gpaControl?.setValue('4.0');
-      expect(gpaControl?.valid).toBeTrue();
-
-      gpaControl?.setValue('5.0');
-      expect(gpaControl?.valid).toBeTrue();
-
-      gpaControl?.setValue('abc');
-      expect(gpaControl?.valid).toBeFalse();
-    });
+  it('marks non-existent major invalid', () => {
+    majorControl.setValue({ operation: 'Create', name: 'Not a Real Major', isMinor: false });
+    expect(majorControl.hasError('invalidMajor')).toBeTrue();
   });
 
   it('should submit properly', () => {
