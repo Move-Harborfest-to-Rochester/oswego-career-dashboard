14148ba6633ad2ce8a63e261e1063d9299fb3ee7
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 13:45:31 -0500

    removed existing support faculty service methods

--- backend/src/main/java/com/senior/project/backend/supportfaculty/SupportFacultyService.java
+++ backend/src/main/java/com/senior/project/backend/supportfaculty/SupportFacultyService.java
@@ -1,13 +1,7 @@
 package com.senior.project.backend.supportfaculty;
 
-import com.senior.project.backend.domain.SupportFaculty;
-import com.senior.project.backend.util.NonBlockingExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 @Service
 public class SupportFacultyService {
@@ -15,17 +9,5 @@ public class SupportFacultyService {
 
     public SupportFacultyService(@Autowired SupportFacultyRepository supportFacultyRepository) {
         this.supportFacultyRepository = supportFacultyRepository;
-    }
-
-    private Flux<SupportFaculty> allSupportFaculty() {
-        return NonBlockingExecutor.executeMany(supportFacultyRepository::findAll);
-    }
-
-    private void deleteSupportFaculty(UUID supportFacultyId) {
-        supportFacultyRepository.deleteById(supportFacultyId);
-    }
-
-    private Mono<SupportFaculty> saveSupportFaculty(SupportFaculty supportFaculty) {
-        return NonBlockingExecutor.execute(() -> supportFacultyRepository.saveAndFlush(supportFaculty));
     }
 }
