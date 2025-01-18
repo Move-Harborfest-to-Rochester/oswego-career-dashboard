64ad60dea31a3ae095140dee2629250229393ff1
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 15:00:30 -0500

    added migration script for basac office faculty

---
+++ backend/src/main/resources/db/migration/V34__BasacOfficeFaculty.sql
@@ -0,0 +1,8 @@
+CREATE TABLE basac_office_faculty
+(
+    id    BINARY(16)   NOT NULL,
+    name  VARCHAR(255) NULL,
+    title VARCHAR(255) NULL,
+    email VARCHAR(255) NULL,
+    CONSTRAINT pk_basacofficefaculty PRIMARY KEY (id)
+);
