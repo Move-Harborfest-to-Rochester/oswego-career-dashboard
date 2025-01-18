a1ffa5269f03861e5a1176767b59d66f12ec0ec0
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 14:51:51 -0500

    added getters on all fields and setters on applicable fields of basac faculty

--- backend/src/main/java/com/senior/project/backend/domain/BasacOfficeFaculty.java
+++ backend/src/main/java/com/senior/project/backend/domain/BasacOfficeFaculty.java
@@ -5,19 +5,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class BasacOfficeFaculty {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Setter
     private String name;
+    @Setter
     private String title;
+    @Setter
     private String email;
 }
