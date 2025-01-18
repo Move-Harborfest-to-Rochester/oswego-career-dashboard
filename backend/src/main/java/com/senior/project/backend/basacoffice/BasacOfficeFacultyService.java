74587f2f18271eeaa68f900173e76a9d33c5cc4d
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 14:06:34 -0500

    added get all to basac service

--- backend/src/main/java/com/senior/project/backend/basacoffice/BasacOfficeFacultyService.java
+++ backend/src/main/java/com/senior/project/backend/basacoffice/BasacOfficeFacultyService.java
@@ -4,6 +4,7 @@ import com.senior.project.backend.domain.BasacOfficeFaculty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -12,6 +13,10 @@ public class BasacOfficeFacultyService {
 
     public BasacOfficeFacultyService(@Autowired BasacOfficeFacultyRepository basacOfficeFacultyRepository) {
         this.basacOfficeFacultyRepository = basacOfficeFacultyRepository;
+    }
+
+    public List<BasacOfficeFaculty> getAllBasacOfficeFaculty() {
+        return basacOfficeFacultyRepository.findAll();
     }
 
     public BasacOfficeFaculty saveBasacOfficeFaculty(BasacOfficeFaculty basacOfficeFaculty) {
