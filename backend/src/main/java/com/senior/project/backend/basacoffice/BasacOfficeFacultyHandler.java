8f2fb165a8c6f93fbd70e0fc51e9d86878b00c99
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 15:00:20 -0500

    fixed bad response in update basac handler, added error handling

--- backend/src/main/java/com/senior/project/backend/basacoffice/BasacOfficeFacultyHandler.java
+++ backend/src/main/java/com/senior/project/backend/basacoffice/BasacOfficeFacultyHandler.java
@@ -1,7 +1,10 @@
 package com.senior.project.backend.basacoffice;
 
 import com.senior.project.backend.common.models.Patch;
+import com.senior.project.backend.domain.BasacOfficeFaculty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -20,8 +23,13 @@ public class BasacOfficeFacultyHandler {
     }
 
     public Mono<ServerResponse> updateBasacOffice(ServerRequest request) {
-        return request.bodyToMono(Patch.class)
-                .flatMap(basacOfficeFacultyService::patchBasacOffice)
-                .flatMap((basacOfficeFaculty) -> ServerResponse.ok().bodyValue(basacOfficeFaculty));
+        return request.bodyToMono(new ParameterizedTypeReference<Patch<BasacOfficeFaculty>>() {})
+                .flatMap(patch -> ServerResponse.ok().bodyValue(basacOfficeFacultyService.patchBasacOffice(patch)))
+                .onErrorResume(error -> {
+                    if (error instanceof IllegalArgumentException) {
+                        return ServerResponse.badRequest().bodyValue(error.getMessage());
+                    }
+                    return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                });
     }
 }
