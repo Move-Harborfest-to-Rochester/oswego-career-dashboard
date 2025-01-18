58832bd14fb14742e71c358e6dfe80d8b5e0a18f
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-18 13:33:52 -0500

    removed support faculty migration for now

--- backend/src/main/resources/db/migration/V00034_SupportFaculty.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE support_faculty
-(
-    id    binary(16)   not null
-        primary key,
-    name  varchar(255) not null,
-    title varchar(255) not null,
-    email varchar(255) not null
-);
-
