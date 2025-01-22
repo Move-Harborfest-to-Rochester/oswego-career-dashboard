e8d19c945ea7101ca2528085191d764b797f0e1a
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-22 18:33:22 -0500

    changed form selector

--- frontend/src/app/admin/edit-support-faculty-form/edit-support-faculty-form.component.ts
+++ frontend/src/app/admin/edit-support-faculty-form/edit-support-faculty-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-support-faculty-form',
+  selector: 'edit-support-faculty-form',
   templateUrl: './edit-support-faculty-form.component.html',
   styleUrls: ['./edit-support-faculty-form.component.less']
 })
