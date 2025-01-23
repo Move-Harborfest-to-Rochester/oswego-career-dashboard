a15eca7cf84324e54d91266d599a80f12b3ab7be
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-22 19:06:36 -0500

    added service call to log to console for now

--- frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.ts
+++ frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup} from "@angular/forms";
+import {BasacFacultyService} from "./basac-faculty.service";
 
 @Component({
   selector: 'edit-basac-faculty-form',
   templateUrl: './edit-basac-faculty-form.component.html',
   styleUrls: ['./edit-basac-faculty-form.component.less']
 })
-export class EditBasacFacultyFormComponent {
+export class EditBasacFacultyFormComponent implements OnInit {
   form: FormGroup = new FormGroup([]);
 
-  constructor() {
+  constructor(private readonly service: BasacFacultyService) {
+  }
+
+  ngOnInit() {
+    this.service.getAll().subscribe(faculties => {
+      console.log(faculties);
+    })
   }
 
   submit() {
