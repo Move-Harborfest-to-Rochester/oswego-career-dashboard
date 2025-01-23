55648882786a1895ebabd5a1a387acfb15f90f68
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-22 19:35:45 -0500

    form fills in with existing faculty, need to add edit functionality

--- frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.ts
+++ frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {BasacFacultyService} from "./basac-faculty.service";
+import {BasacFaculty} from "../../../domain/BasacFaculty";
 
 @Component({
   selector: 'edit-basac-faculty-form',
@@ -8,23 +9,46 @@ import {BasacFacultyService} from "./basac-faculty.service";
   styleUrls: ['./edit-basac-faculty-form.component.less']
 })
 export class EditBasacFacultyFormComponent implements OnInit {
-  form: FormGroup = new FormGroup([]);
+  form: FormGroup = this.formBuilder.group({
+    faculty: this.formBuilder.array([])
+  });
 
-  constructor(private readonly service: BasacFacultyService) {
+  constructor(private readonly formBuilder: FormBuilder, private readonly service: BasacFacultyService) {
   }
 
   ngOnInit() {
     this.service.getAll().subscribe(faculties => {
-      console.log(faculties);
+      this.form = this.formBuilder.group({
+        faculty: this.formBuilder.array(
+          faculties.map(faculty => this.formBuilder.group({
+            op: this.formBuilder.control('replace'),
+            id: this.formBuilder.control(faculty.id),
+            name: this.formBuilder.control(faculty.name),
+            title: this.formBuilder.control(faculty.title),
+            email: this.formBuilder.control(faculty.email)
+          }))
+        )
+      });
     })
+  }
+
+  facultyControls(): AbstractControl[] {
+    return (this.form.get('faculty') as FormArray).controls;
+  }
+
+  getControlFrom(facultyControl: AbstractControl, name: string): FormControl {
+    return facultyControl.get(name) as FormControl;
   }
 
   submit() {
 
   }
 
-  deleteFaculty() {
-
+  deleteFacultyAtIndex(facultyControl: FormControl, index: number) {
+    faculty.removeAt(index);
+    if (facultyControl.get('op').value === 'add') {
+      return;
+    }
   }
 
   addNewFaculty() {
