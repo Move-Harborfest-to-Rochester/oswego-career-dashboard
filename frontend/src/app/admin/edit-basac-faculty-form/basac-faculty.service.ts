4922666d4711b75157a1a9467caec2c8b10ed402
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-22 19:03:50 -0500

    added http request to getAll in basac service

--- frontend/src/app/admin/edit-basac-faculty-form/basac-faculty.service.ts
+++ frontend/src/app/admin/edit-basac-faculty-form/basac-faculty.service.ts
@@ -1,39 +1,20 @@
 import { Injectable } from '@angular/core';
-
-interface BasacFacultyJSON {
-  id: string;
-  name: string;
-  title: string;
-  email: string;
-}
-
-class BasacFaculty {
-  private readonly id: string;
-  private readonly name: string;
-  private readonly title: string;
-  private readonly email: string;
-
-  constructor(json: BasacFacultyJSON) {
-    this.id = json.id;
-    this.name = json.name;
-    this.title = json.title;
-    this.email = json.email;
-  }
-}
+import {HttpClient} from "@angular/common/http";
+import {constructBackendRequest, Endpoints} from "../../util/http-helper";
+import {map, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasacFacultyService {
+  constructor(private readonly http: HttpClient) { }
 
-  constructor() { }
-
-  getAll(): BasacFaculty[] {
-    return [
-      new BasacFaculty({ id: '1', name: 'Name 1', title: 'Title 1', email: 'Email 1' }),
-      new BasacFaculty({ id: '2', name: 'Name 2', title: 'Title 2', email: 'Email 2' }),
-      new BasacFaculty({ id: '3', name: 'Name 3', title: 'Title 3', email: 'Email 3' }),
-      new BasacFaculty({ id: '4', name: 'Name 4', title: 'Title 4', email: 'Email 4' }),
-    ];
+  getAll(): Observable<BasacFaculty[]> {
+    const url = constructBackendRequest(Endpoints.GET_ALL_BASAC_FACULTY);
+    return this.http.get<BasacFacultyJSON[]>(url).pipe(
+      map(
+        basacFaculty => basacFaculty.map(faculty => new BasacFaculty(faculty))
+      )
+    );
   }
 }
