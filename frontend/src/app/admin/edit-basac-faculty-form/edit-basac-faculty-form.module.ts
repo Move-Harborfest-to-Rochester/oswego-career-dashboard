5d524015a2f73ffd7ccd2b52ee4579725a7a8b05
Collin Giess <cgiess@wilmactech.com>
2025-01-29 20:24:14 -0500

    style: wrapped each set of faculty inputs in a card

--- frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.module.ts
+++ frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EditBasacFacultyFormComponent } from './edit-basac-faculty-form.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+  EditBasacFacultyFormComponent
+} from './edit-basac-faculty-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {HttpClientModule} from "@angular/common/http";
-
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
@@ -21,10 +23,12 @@ import {HttpClientModule} from "@angular/common/http";
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   exports: [
     EditBasacFacultyFormComponent
   ]
 })
-export class EditBasacFacultyFormModule { }
+export class EditBasacFacultyFormModule {
+}
