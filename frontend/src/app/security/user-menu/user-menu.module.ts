bd3c9e54dd113dbfaecb341363d933423c2f6b19
Collin Giess <cgiess@wilmactech.com>
2025-01-30 17:32:21 -0500

    changed icon to a toggle switch

--- frontend/src/app/security/user-menu/user-menu.module.ts
+++ frontend/src/app/security/user-menu/user-menu.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { UserMenuComponent } from './user-menu.component';
-import { MatButtonModule } from '@angular/material/button';
+import {UserMenuComponent} from './user-menu.component';
+import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from "@angular/material/menu";
 import {MatIconModule} from "@angular/material/icon";
-import { RouterModule } from '@angular/router';
-
+import {RouterModule} from '@angular/router';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -18,8 +19,11 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatSlideToggleModule,
+    FormsModule
   ],
   exports: [UserMenuComponent]
 })
-export class UserMenuModule { }
+export class UserMenuModule {
+}
