eab7a9d5d3a3a6b099cefbecf83c7464abb5fc9d
Collin Giess <cgiess@wilmactech.com>
2025-01-29 20:25:30 -0500

    test: added card import to basac form

--- frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.spec.ts
+++ frontend/src/app/admin/edit-basac-faculty-form/edit-basac-faculty-form.component.spec.ts
@@ -11,6 +11,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
 
 describe('EditBasacFacultyFormComponent', () => {
   let component: EditBasacFacultyFormComponent;
@@ -27,6 +28,7 @@ describe('EditBasacFacultyFormComponent', () => {
         MatInputModule,
         MatButtonModule,
         MatIconModule,
+        MatCardModule,
       ],
       providers: [
         {
