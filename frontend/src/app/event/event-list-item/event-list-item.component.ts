c91c2ea418827277f8af3444646a846b8643cd66
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-01 13:02:27 -0500

    added event list item card with link to event page

--- frontend/src/app/event/event-list-item/event-list-item.component.ts
+++ frontend/src/app/event/event-list-item/event-list-item.component.ts
@@ -1,12 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Event} from "../../../domain/Event";
+import {ArtifactService} from 'src/app/file-upload/artifact.service';
 
 @Component({
   selector: 'event-list-item',
   templateUrl: './event-list-item.component.html',
   styleUrls: ['./event-list-item.component.less']
 })
-export class EventListItemComponent {
+export class EventListItemComponent implements OnInit {
   @Input() event!: Event;
+  imageUrl?: string;
 
+  constructor(private readonly artifactService: ArtifactService) {}
+
+  ngOnInit() {
+    if (this.event.imageId == null) return;
+    this.imageUrl = this.artifactService.getEventImageUrl(this.event.imageId);
+  }
+
+  goToEvent() {
+    window.open(this.event.eventLink, "_blank");
+  }
 }
