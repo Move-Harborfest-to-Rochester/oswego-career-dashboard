454ddc4e73b114b8e2639e60235fd024ee4da36d
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-01 12:35:02 -0500

    created event module and event list component

---
+++ frontend/src/app/event/event.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventListComponent } from './event-list/event-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    EventListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    EventListComponent
+  ]
+})
+export class EventModule { }
