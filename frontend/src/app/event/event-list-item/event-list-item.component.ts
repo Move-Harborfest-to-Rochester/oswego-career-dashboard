4ce92e649310d78739a3799c173910177a1203dc
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-01 12:35:58 -0500

    created an event list item component

---
+++ frontend/src/app/event/event-list-item/event-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list-item',
+  templateUrl: './event-list-item.component.html',
+  styleUrls: ['./event-list-item.component.less']
+})
+export class EventListItemComponent {
+
+}
--- frontend/src/app/event/event.module.ts
+++ frontend/src/app/event/event.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventListComponent } from './event-list/event-list.component';
+import { EventListItemComponent } from './event-list-item/event-list-item.component';
 
 
 
 @NgModule({
   declarations: [
-    EventListComponent
+    EventListComponent,
+    EventListItemComponent
   ],
   imports: [
     CommonModule
