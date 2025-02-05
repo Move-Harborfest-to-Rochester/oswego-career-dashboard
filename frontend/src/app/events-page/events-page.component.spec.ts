cdcc0d5094a7e8eba7e364a9523ad1fcc4639d1b
Himel Uddin <himelmuddin@gmail.com>
2025-02-04 19:16:14 -0500

    Test for events page

--- frontend/src/app/events-page/events-page.component.spec.ts
+++ frontend/src/app/events-page/events-page.component.spec.ts
@@ -1,13 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { EventsPageComponent } from './events-page.component';
 
-describe('EventsPageComponentComponent', () => {
+describe('EventsPageComponent', () => {
   let component: EventsPageComponent;
   let fixture: ComponentFixture<EventsPageComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatProgressSpinnerModule
+      ],
       declarations: [EventsPageComponent]
     });
     fixture = TestBed.createComponent(EventsPageComponent);
