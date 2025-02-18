9f3edb80af2f5db5f32d1fb1f2c2303c1d33f3e9
Aaron <110870409+asan6602@users.noreply.github.com>
2025-02-18 16:28:16 -0500

    more text fixes

--- frontend/src/app/admin/analytics-dashboard/analytics-dashboard.component.spec.ts
+++ frontend/src/app/admin/analytics-dashboard/analytics-dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import HttpClientTestingModule here
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms'; 
 import { AnalyticsDashboardComponent } from './analytics-dashboard.component';
 
 describe('AnalyticsDashboardComponent', () => {
@@ -8,7 +9,7 @@ describe('AnalyticsDashboardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], // Add HttpClientTestingModule here
+      imports: [HttpClientTestingModule, FormsModule], // Add FormsModule here
       declarations: [AnalyticsDashboardComponent]
     });
     fixture = TestBed.createComponent(AnalyticsDashboardComponent);
