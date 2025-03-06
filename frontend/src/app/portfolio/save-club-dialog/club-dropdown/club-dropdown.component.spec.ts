f50db10c524c7fce81122b257e7a249ce8ccc510
Collin Giess <cgiess@wilmactech.com>
2025-03-06 15:33:06 -0500

    fix failing tests

--- frontend/src/app/portfolio/save-club-dialog/club-dropdown/club-dropdown.component.spec.ts
+++ frontend/src/app/portfolio/save-club-dialog/club-dropdown/club-dropdown.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClubDropdownComponent } from './club-dropdown.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ClubDropdownComponent', () => {
   let component: ClubDropdownComponent;
@@ -8,10 +13,14 @@ describe('ClubDropdownComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ClubDropdownComponent]
+      declarations: [ClubDropdownComponent],
+      imports: [NoopAnimationsModule, MatInputModule, MatFormFieldModule, MatAutocompleteModule, ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(ClubDropdownComponent);
     component = fixture.componentInstance;
+    component.clubControl = new FormControl<string>('', [
+      Validators.required,
+    ]) as FormControl<string>;
     fixture.detectChanges();
   });
 
