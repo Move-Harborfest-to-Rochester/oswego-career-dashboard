3150c2e231ace7c21a67aa8237486017fc268545
Greg Villafane <69919565+gregSWEN@users.noreply.github.com>
2025-03-06 09:53:54 -0500

    Add club dropdown (#54)
    * drop down working
    
    * passing tests
    
    * remove comments, adjust styling
    
    ---------
    
    Co-authored-by: Greg Villafane <gjv4512@g.rit.edu>

---
+++ frontend/src/app/util/dropdown-constants.ts
@@ -0,0 +1,24 @@
+export const allMajors: string[] = [
+  'Accounting',
+  'Business Administration',
+  'Finance',
+  'Human Resource Management',
+  'Marketing',
+  'Operations Management and Information Systems',
+  'Risk Management and Insurance',
+];
+export const allClubs: string[] =[
+  "Accounting Society",
+  "Beta Alpha Psi",
+  "American Marketing Association (AMA)",
+  "Collegiate Entrepreneurs’ Organization (CEO)",
+  "Financial Management Association (FMA)",
+  "Investment Club",
+  "National Association of Black Accountants (NABA)",
+  "Phi Beta Lambda (PBL)",
+  "Real Estate Club",
+  "Risk Management and Insurance Club",
+  "Society for Human Resource Management (SHRM)",
+  "Supply Chain and Operations Management (SCOPE)",
+  "Women in Business"
+];
--- frontend/src/app/util/major-list.ts
+++
@@ -1,9 +0,0 @@
-export const allMajors: string[] = [
-  'Accounting',
-  'Business Administration',
-  'Finance',
-  'Human Resource Management',
-  'Marketing',
-  'Operations Management and Information Systems',
-  'Risk Management and Insurance',
-];
