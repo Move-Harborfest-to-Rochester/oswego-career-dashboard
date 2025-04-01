a6e73e8a2465da9541dea86526035ac5195c1fdb
Zachary Montgomery <zsm5532@rit.edu>
2025-04-01 12:31:38 -0400

    Remove student links from non student views

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,9 +1,28 @@
-import {Component} from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { AuthService } from '../security/auth.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Role, User } from '../security/domain/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.less']
 })
-export class HomepageComponent{
+export class HomepageComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+
+  showHotlinks: boolean = false;
+
+  constructor(
+    public readonly authService: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.user$.pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((user: User | null) => {
+      this.showHotlinks = (user?.role == Role.Student);
+    });
+  }
 }
