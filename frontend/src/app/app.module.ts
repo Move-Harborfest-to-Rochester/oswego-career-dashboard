5454219244030cb732c76d04ef7ec785c067b0e3
Collin Giess <cgiess@wilmactech.com>
2025-04-02 15:09:40 -0400

    add admin ability to edit student's year levels

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,60 +2,83 @@ import {
   GoogleLoginProvider,
   SocialLoginModule
 } from '@abacritt/angularx-social-login';
-import { NgOptimizedImage } from '@angular/common';
+import {NgOptimizedImage} from '@angular/common';
 import {
   HTTP_INTERCEPTORS,
   HttpClientModule,
   provideHttpClient
 } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
-import { MatTabsModule } from '@angular/material/tabs';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
-import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { environment } from 'src/environments/environment';
-import { EventImageModalModule } from './admin/event-image-modal/event-image-modal.module';
-import { EventMainPageModule } from './admin/event-main-page/event-main-page.module';
-import { MilestoneEditModule } from './admin/milestone-edit/milestone-edit.module';
-import { MilestoneCreateModalModule } from './admin/milestone-main-page/milestone-create-modal/milestone-create-modal.module';
-import { MilestoneMainPageModule } from './admin/milestone-main-page/milestones-main-page.module';
-import { TaskEditModalModule } from './admin/task-edit-modal/task-edit-modal.module';
-import { TaskMainPageModule } from './admin/task-main-page/task-main-page.module';
-import { ApiDocumentationsComponent } from './api-documentations/api-documentations.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageModule } from './homepage/homepage.module';
-import { FileUploadModule } from './file-upload/file-upload.module';
-import { LogoLinkModule } from './logo-link/logo-link.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { OswegoLogoModule } from './oswego-logo/oswego-logo.module';
-import { PortfolioModule } from './portfolio/portfolio.module';
-import { AuthService } from './security/auth.service';
-import { AuthInterceptor } from './security/interceptors/auth-interceptor';
-import { LoginPageModule } from './security/login-page/login-page.module';
-import { SignupPageModule } from './security/signup-page/signup-page.module';
-import { UserMenuModule } from './security/user-menu/user-menu.module';
-import { TasksModalModule } from './tasks-modal/tasks-modal.module';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { FooterModule } from './footer/footer.module';
-import { MatIconModule } from '@angular/material/icon';
-import { ProfileImageModalModule } from './file-upload/profile-image-modal/profile-image-modal.module';
-import { FormsModule } from '@angular/forms';
-import { NgChartsModule } from 'ng2-charts';
-import { SettingsPageModule } from './settings/settings-page.module';import { MilestonesFacultyModule } from './milestones-page/milestones-faculty/milestones-faculty.module';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {MsalModule, MsalRedirectComponent} from '@azure/msal-angular';
+import {InteractionType, PublicClientApplication} from '@azure/msal-browser';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
+import {environment} from 'src/environments/environment';
+import {
+  EventImageModalModule
+} from './admin/event-image-modal/event-image-modal.module';
+import {
+  EventMainPageModule
+} from './admin/event-main-page/event-main-page.module';
+import {
+  MilestoneEditModule
+} from './admin/milestone-edit/milestone-edit.module';
+import {
+  MilestoneCreateModalModule
+} from './admin/milestone-main-page/milestone-create-modal/milestone-create-modal.module';
+import {
+  MilestoneMainPageModule
+} from './admin/milestone-main-page/milestones-main-page.module';
+import {
+  TaskEditModalModule
+} from './admin/task-edit-modal/task-edit-modal.module';
+import {TaskMainPageModule} from './admin/task-main-page/task-main-page.module';
+import {
+  ApiDocumentationsComponent
+} from './api-documentations/api-documentations.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomepageModule} from './homepage/homepage.module';
+import {FileUploadModule} from './file-upload/file-upload.module';
+import {LogoLinkModule} from './logo-link/logo-link.module';
+import {NavbarComponent} from './navbar/navbar.component';
+import {OswegoLogoModule} from './oswego-logo/oswego-logo.module';
+import {PortfolioModule} from './portfolio/portfolio.module';
+import {AuthService} from './security/auth.service';
+import {AuthInterceptor} from './security/interceptors/auth-interceptor';
+import {LoginPageModule} from './security/login-page/login-page.module';
+import {SignupPageModule} from './security/signup-page/signup-page.module';
+import {UserMenuModule} from './security/user-menu/user-menu.module';
+import {TasksModalModule} from './tasks-modal/tasks-modal.module';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {FooterModule} from './footer/footer.module';
+import {MatIconModule} from '@angular/material/icon';
+import {
+  ProfileImageModalModule
+} from './file-upload/profile-image-modal/profile-image-modal.module';
+import {FormsModule} from '@angular/forms';
+import {NgChartsModule} from 'ng2-charts';
+import {SettingsPageModule} from './settings/settings-page.module';
+import {
+  MilestonesFacultyModule
+} from './milestones-page/milestones-faculty/milestones-faculty.module';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-import { AnalyticsDashboardComponent } from './admin/analytics-dashboard/analytics-dashboard.component';
-import { TasksModule } from './tasks/tasks.module';
-import { UsersPageModule } from './users-page/users-page.module';
-import {EditBasacFacultyModule} from "./admin/edit-basac-faculty/edit-basac-faculty.module";
+import {
+  AnalyticsDashboardComponent
+} from './admin/analytics-dashboard/analytics-dashboard.component';
+import {TasksModule} from './tasks/tasks.module';
+import {UsersPageModule} from './users-page/users-page.module';
+import {
+  EditBasacFacultyModule
+} from "./admin/edit-basac-faculty/edit-basac-faculty.module";
 import {EventModule} from "./event/event.module";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {EventsPageComponent} from "./events-page/events-page.component";
@@ -153,7 +176,7 @@ import {EventsPageComponent} from "./events-page/events-page.component";
         ]
       }
     },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     {
       provide: APP_INITIALIZER,
       useFactory: (authService: AuthService) => () => authService.loadUser(),
@@ -163,4 +186,5 @@ import {EventsPageComponent} from "./events-page/events-page.component";
   ],
   bootstrap: [AppComponent, MsalRedirectComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- frontend/src/app/users-page/users-page.module.ts
+++ frontend/src/app/users-page/users-page.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { UsersPageComponent } from './users-page.component';
+import {UsersPageComponent} from './users-page.component';
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
@@ -9,13 +9,20 @@ import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
-import { EditRoleMenuModule } from './edit-role-menu/edit-role-menu.module';
+import {EditRoleMenuModule} from './edit-role-menu/edit-role-menu.module';
 import {MatSortModule} from "@angular/material/sort";
 import {MatSelectModule} from "@angular/material/select";
+import {EditYearMenuComponent} from './edit-year-menu/edit-year-menu.component';
+import {
+  EditYearConfirmationDialogComponent
+} from './edit-year-menu/edit-year-confirmation-dialog/edit-year-confirmation-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
     UsersPageComponent,
+    EditYearMenuComponent,
+    EditYearConfirmationDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -30,7 +37,9 @@ import {MatSelectModule} from "@angular/material/select";
     MatMenuModule,
     EditRoleMenuModule,
     MatSortModule,
-    MatSelectModule
+    MatSelectModule,
+    MatDialogModule
   ],
 })
-export class UsersPageModule { }
+export class UsersPageModule {
+}
